4a288196fbbe6662612c544be1310cfe1a054615
Phaniraj <79626160+phani-training@users.noreply.github.com>
2023-07-26 18:54:12 +0530

    Add files via upload

---
+++ Microservices/CoreAppWithDocker/Controllers/CustomerController.cs
@@ -0,0 +1,56 @@
+﻿using CoreAppWithDocker.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreAppWithDocker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly CustomerContext _context;
+        public CustomerController(CustomerContext ctx)
+        {
+            _context = ctx;  
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Customer>> GetCustomers() => _context.Customers;
+
+        [HttpGet("{customerId:int}")]
+        public async Task<ActionResult<Customer>> GetById(int customerId)
+        {
+            return await _context.Customers.FindAsync(customerId);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddNewCustomer(Customer cst)
+        {
+            await _context.Customers.AddAsync(cst);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateCustomer(Customer cst)
+        {
+            _context.Customers.Update(cst);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{customerId:int}")]
+        public async Task<ActionResult> Delete(int customerId)
+        {
+            var cst = await _context.Customers.FindAsync(customerId);
+            if(cst == null)
+            {
+                return BadRequest("Customer is not found");
+            }
+            _context.Customers.Remove(cst);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ Microservices/CoreAppWithDocker/Models/Customer.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoreAppWithDocker.Models
+{
+    [Table("tblCustomer", Schema ="dbo")]
+    public class Customer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CustomerId { get; set; }
+
+        [Column("customername")]
+        public string CustomerName { get; set; } = string.Empty;
+
+        [Column("customeremail")]
+        public string CustomerEmail { get; set; } = string.Empty;
+
+        [Column("customerphone")]
+        public long CustomerPhone { get; set; }
+    }
+
+    public class CustomerContext : DbContext
+    {
+        public CustomerContext(DbContextOptions<CustomerContext> dbOptions) : base(dbOptions)
+        {
+            try
+            {
+                var dbCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+                if(dbCreator != null)
+                {
+                    if (!dbCreator.CanConnect()) dbCreator.Create();
+                    if (!dbCreator.HasTables()) dbCreator.CreateTables();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
